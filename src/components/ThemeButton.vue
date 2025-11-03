277c556e4cd75e13d7deb1af257da90e2d24943a
jaminleod <jaminleod@pm.me>
2025-11-03 02:00:27 -0500

    initial commit
    all my favourite basic styling is here :)

---
+++ src/components/ThemeButton.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+	import {ref, computed} from 'vue'
+
+	const themes = ref(['light', 'dark'])
+	const currentIndex = ref(0)
+
+	const currentTheme = computed(() => themes.value[currentIndex.value])
+	const nextTheme = computed(() => themes.value[(currentIndex.value + 1) % themes.value.length])
+	const displayTheme = computed(() =>
+		nextTheme.value.charAt(0).toUpperCase() + nextTheme.value.slice(1) + ' Theme'
+	)
+
+	function toggle() {
+		document.body.classList.remove(currentTheme.value)
+		currentIndex.value = (currentIndex.value + 1) % themes.value.length
+		document.body.classList.add(currentTheme.value)
+	}
+
+	function addTheme(themeName: string) {
+		if (!themes.value.includes(themeName)) {
+			themes.value.push(themeName)
+		}
+	}
+
+	defineExpose({addTheme})
+</script>
+
+<template>
+	<button @click="toggle" class="theme-button">
+		{{ displayTheme }}
+	</button>
+</template>
